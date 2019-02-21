b3e8a92c27bf1e14a3e775d2a45904a5ff01e78c
Julian Fischer <myst3rion9@gmail.com>
2019-02-21 23:42:59 +0100

    cpu_input_boost: Create separate tunable for Dynamic Schedtune Boost duration
    Based on: https://github.com/joshuous/android_kernel_oneplus_msm8998/commit/7a1d4588637de31db5167d3a634163693d7d9258

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -17,6 +17,8 @@ static bool stune_boost_active;
 static int boost_slot;
 static unsigned short dynamic_stune_boost;
 module_param(dynamic_stune_boost, short, 0644);
+static unsigned int dynamic_stune_boost_ms = 40;
+module_param(dynamic_stune_boost_ms, uint, 0644);
 #endif
 
 /* Available bits for boost_drv state */
@@ -29,6 +31,9 @@ struct boost_drv {
 	struct workqueue_struct *wq;
 	struct work_struct input_boost;
 	struct delayed_work input_unboost;
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+        struct delayed_work stune_unboost;
+#endif
 	struct work_struct max_boost;
 	struct delayed_work max_unboost;
 	struct notifier_block cpu_notif;
@@ -88,6 +93,15 @@ static void unboost_all_cpus(struct boost_drv *b)
 		!cancel_delayed_work_sync(&b->max_unboost))
 		return;
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+        cancel_delayed_work_sync(&b->stune_unboost);
+
+        if (stune_boost_active) {
+                reset_stune_boost("top-app", boost_slot);
+                stune_boost_active = false;
+        }
+#endif
+
 	clear_boost_bit(b, INPUT_BOOST | WAKE_BOOST | MAX_BOOST);
 	update_online_cpu_policy();
 }
@@ -141,8 +155,13 @@ static void input_boost_worker(struct work_struct *work)
 	}
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
+        cancel_delayed_work_sync(&b->stune_unboost);
+
 	if (!do_stune_boost("top-app", dynamic_stune_boost, &boost_slot))
 		stune_boost_active = true;
+
+        queue_delayed_work(b->wq, &b->stune_unboost,
+                msecs_to_jiffies(dynamic_stune_boost_ms));
 #endif
 	queue_delayed_work(b->wq, &b->input_unboost,
 		msecs_to_jiffies(CONFIG_INPUT_BOOST_DURATION_MS));
@@ -154,14 +173,21 @@ static void input_unboost_worker(struct work_struct *work)
 		container_of(to_delayed_work(work), typeof(*b), input_unboost);
 
 	clear_boost_bit(b, INPUT_BOOST);
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	if (stune_boost_active) {
-		reset_stune_boost("top-app", boost_slot);
-		stune_boost_active = false;
-	}
-#endif
 	update_online_cpu_policy();
 }
+
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static void stune_unboost_worker(struct work_struct *work)
+{
+        struct boost_drv *b =
+                container_of(to_delayed_work(work), typeof(*b), stune_unboost);
+
+        if (stune_boost_active) {
+                reset_stune_boost("top-app", boost_slot);
+                stune_boost_active = false;  
+        }
+}
+#endif
 
 static void max_boost_worker(struct work_struct *work)
 {
@@ -173,8 +199,13 @@ static void max_boost_worker(struct work_struct *work)
 	}
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
+        cancel_delayed_work_sync(&b->stune_unboost);
+
 	if (!do_stune_boost("top-app", dynamic_stune_boost, &boost_slot))
 		stune_boost_active = true;
+	
+        queue_delayed_work(b->wq, &b->stune_unboost,
+                msecs_to_jiffies(atomic_read(&b->max_boost_dur)));
 #endif
 	queue_delayed_work(b->wq, &b->max_unboost,
 		msecs_to_jiffies(atomic_read(&b->max_boost_dur)));
@@ -186,12 +217,6 @@ static void max_unboost_worker(struct work_struct *work)
 		container_of(to_delayed_work(work), typeof(*b), max_unboost);
 
 	clear_boost_bit(b, WAKE_BOOST | MAX_BOOST);
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	if (stune_boost_active) {
-		reset_stune_boost("top-app", boost_slot);
-		stune_boost_active = false;
-	}
-#endif
 	update_online_cpu_policy();
 }
 
@@ -300,12 +325,6 @@ free_handle:
 
 static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 {
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	if (stune_boost_active) {
-		reset_stune_boost("top-app", boost_slot);
-		stune_boost_active = false;
-	}
-#endif
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
@@ -363,6 +382,9 @@ static int __init cpu_input_boost_init(void)
 	atomic64_set(&b->max_boost_expires, 0);
 	INIT_WORK(&b->input_boost, input_boost_worker);
 	INIT_DELAYED_WORK(&b->input_unboost, input_unboost_worker);
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+        INIT_DELAYED_WORK(&b->stune_unboost, stune_unboost_worker);
+#endif
 	INIT_WORK(&b->max_boost, max_boost_worker);
 	INIT_DELAYED_WORK(&b->max_unboost, max_unboost_worker);
 	atomic_set(&b->state, 0);
